6827042c14a158be5e7f0d4f5dc6f35856cc4009
Kome-to <ducanhnd2306@gmail.com>
2022-08-15 19:39:54 +0700

    update

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,12 +3,14 @@ const modal = document.querySelector('.js-modal');
 const closeBtn = document.querySelector('.js-modal-close');
 const modalTable = document.querySelector('.js-modal-table');
 
-function openModal() {
-    modal.classList.add('open');
-}
+
 
 function hideModal() {
     modal.classList.remove('open');
+}
+
+function openModal() {
+    modal.classList.add('open');
 }
 
 for (const buyBtn of buyBtns) {
